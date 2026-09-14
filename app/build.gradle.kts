a01a7efd690e7dbfd3da37d6820c748212ff4a67
Pat0chat <github.brfoy@passmail.com>
2026-09-14 13:35:21 +0200

    Release v1.14.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "com.meteocompare.app"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 34
-        versionName = "1.14.2"
+        versionCode = 35
+        versionName = "1.14.3"
         testInstrumentationRunner = "com.meteocompare.app.HiltTestRunner"
         buildConfigField("String", "METEOCOMPARE_BASE_URL", "\"$vigilanceBaseUrl\"")
         vectorDrawables { useSupportLibrary = true }
